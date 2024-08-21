0541ad8966e8272cbbc947200be5774da8eb6529
Kevin Alvarez <kevinalvarez373@gmail.com>
2024-08-22 05:09:40 +0800

    applied changes to fonts and layout

--- src/components/ui/DragCards/HeroTitle.tsx
+++ src/components/ui/DragCards/HeroTitle.tsx
@@ -7,15 +7,15 @@ interface HeroTitleProps {
 
 const HeroTitle: React.FC<HeroTitleProps> = ({ title, subtitle }) => {
   return (
-    <section className="relative grid min-h-screen w-full place-content-center overflow-hidden flex-row justify-center">
-      <h1 className="relative z-0 text-[48px] font-outfit font-bold text-slate-300 md:text-[48px] whitespace-nowrap">
+    <section className="relative flex min-h-screen w-full flex-col justify-center items-center overflow-hidden">
+      <h1 className="relative z-0 text-[32px] font-outfit font-medium text-slate-300 md:text-[48px] whitespace-nowrap">
         {title}
       </h1>
-      <h2 className="relative z-0 text-[24px] font-gloria text-stone-400 md:text-[24px] whitespace-nowrap">
+      <h2 className="relative z-0 text-[16px] font-gloria text-stone-400 md:text-[24px] whitespace-nowrap">
         {subtitle}
       </h2>
-
     </section>
+
   );
 };
 
